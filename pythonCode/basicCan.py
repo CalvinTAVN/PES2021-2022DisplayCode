35e150f704f9e84f2307623bed9c16423643d988
Silas M <smohr@princeton.edu>
2022-05-25 12:29:18 -0400

    prettier printing

--- pythonCode/basicCan.py
+++ pythonCode/basicCan.py
@@ -7,14 +7,14 @@ os.system("sudo ifconfig can0 up")
 
 can0 = can.interface.Bus(channel='can0', bustype='socketcan')
 for x in range(10):
-	msg = can0.recv(3.0)	
-	print(msg)
-	print(msg.data)
-	print("------------------")
-	for x in msg.data:
-		print(x)
-	print("---------")
-	print(msg.arbitration_id)
+	msg = can0.recv(3.0)
+	print('-----------------------------')
+	print('msg: ' + str(msg))
+	print('arb id: ' + str(msg.arbitration_id))
+	print('data: ' + str(msg.data))
+	for i in msg.data:
+		print('values: ' + str(i))
+	print('-----------------------------')
 #print(msg.data)
 #print(msg.arbitration_id)
 os.system("sudo ifconfig can0 down")
