d6f6ed682037cff2ba95453f698f9e30bcf9e5f0
drewcurran <denisac@princeton.edu>
2022-10-30 09:52:42 -0400

    Create DashboardGUI.py

---
+++ pythonCode/DashboardGUI.py
@@ -0,0 +1,16 @@
+import PyQt6
+
+class DashboardGUI:
+
+    def __init__(self):
+        pass
+
+class SpeedWidget:
+
+    def __init__(self):
+        pass
+
+class TemperatureWidget:
+
+    def __init__(self):
+        pass
